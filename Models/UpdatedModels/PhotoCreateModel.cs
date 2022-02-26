fc6ddf126037a5498b7a971a41f5c470276d7ed7
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-02-26 17:31:41 +0300

    added models for mapping photo model

---
+++ Models/UpdatedModels/PhotoCreateModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoStock.Models.UpdatedModels
+{
+    public class PhotoCreateModel
+    {
+        public string Name { get; set; }
+        public string ContentUrl { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Cost { get; set; }
+        public int PurchaseAmount { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ Models/UpdatedModels/PhotoUpdatedModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoStock.Models.UpdatedModels
+{
+    public class PhotoUpdatedModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContentUrl { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Cost { get; set; }
+        public int PurchaseAmount { get; set; }
+    }
+}
