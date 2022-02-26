6a24424c9d48e0db33dfb7db00af3e8f1eae80f8
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-02-26 17:31:13 +0300

    service for text are added

---
+++ Services/Text/ITextService.cs
@@ -0,0 +1,16 @@
+﻿using PhotoStock.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhotoStock.Services
+{
+    public interface ITextService
+    {
+        Task<IEnumerable<Text>> GetTextsAsync();
+        Task<Text> GetTextByIdAsync(int textId);
+        Task DeleteTextAsync(int textId);
+        Task<Text> InsertTextAsync(Text text);
+        Task SaveAsync();
+        Task UpdateTextAsync(Text text);
+    }
+}
---
+++ Services/Text/TextService.cs
@@ -0,0 +1,47 @@
+﻿using PhotoStock.DAL;
+using PhotoStock.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhotoStock.Services
+{
+    public class TextService : ITextService
+    {
+        private ITextRepository textRepository;
+
+        public TextService(ITextRepository textRepository)
+        {
+            this.textRepository = textRepository;
+        }
+
+        public async Task DeleteTextAsync(int textId)
+        {
+            await textRepository.DeleteTextAsync(textId);
+        }
+
+        public async Task<Text> GetTextByIdAsync(int textId)
+        {
+            return await textRepository.GetTextByIdAsync(textId);
+        }
+
+        public async Task<IEnumerable<Text>> GetTextsAsync()
+        {
+            return await textRepository.GetTextsAsync();
+        }
+
+        public async Task<Text> InsertTextAsync(Text text)
+        {
+            return await textRepository.InsertTextAsync(text);
+        }
+
+        public async Task SaveAsync()
+        {
+            await textRepository.SaveAsync();
+        }
+
+        public async Task UpdateTextAsync(Text text)
+        {
+            await textRepository.UpdateTextAsync(text);
+        }
+    }
+}
