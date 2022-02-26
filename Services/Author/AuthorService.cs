80e86ebe071a8ac221663c59aa3f0ae6f8b9089f
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-02-26 17:30:29 +0300

    services for author are added

---
+++ Services/Author/AuthorService.cs
@@ -0,0 +1,47 @@
+﻿using PhotoStock.DAL;
+using PhotoStock.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhotoStock.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private IAuthorRepository authorRepository;
+
+        public AuthorService(IAuthorRepository authorRepository)
+        {
+            this.authorRepository = authorRepository;
+        }
+
+        public async Task DeleteAuthorAsync(int authorId)
+        {
+            await authorRepository.DeleteAuthorAsync(authorId);
+        }
+
+        public async Task<Author> GetAuthorByIdAsync(int authorId)
+        {
+            return await authorRepository.GetAuthorByIdAsync(authorId);
+        }
+
+        public async Task<IEnumerable<Author>> GetAuthorsAsync()
+        {
+            return await authorRepository.GetAuthorsAsync();
+        }
+
+        public async Task<Author> InsertAuthorAsync(Author author)
+        {
+            return await authorRepository.InsertAuthorAsync(author);
+        }
+
+        public async Task SaveAsync()
+        {
+            await authorRepository.SaveAsync();
+        }
+
+        public async Task UpdateAuthorAsync(Author author)
+        {
+            await authorRepository.UpdateAuthorAsync(author);
+        }
+    }
+}
---
+++ Services/Author/IAuthorService.cs
@@ -0,0 +1,16 @@
+﻿using PhotoStock.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhotoStock.Services
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<Author>> GetAuthorsAsync();
+        Task<Author> GetAuthorByIdAsync(int authorId);
+        Task<Author> InsertAuthorAsync(Author author);
+        Task DeleteAuthorAsync(int authorId);
+        Task UpdateAuthorAsync(Author author);
+        Task SaveAsync();
+    }
+}
