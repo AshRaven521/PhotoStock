e6670e4ccad4e39ea17f500046c4d2715182d5da
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-02-26 17:32:51 +0300

    added mapping configuration

---
+++ Mapping/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using PhotoStock.Models;
+using PhotoStock.Models.UpdatedModels;
+
+namespace PhotoStock.Mapping
+{
+    public class MappingProfiles : AutoMapper.Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Photo, PhotoUpdatedModel>();
+            CreateMap<Photo, PhotoCreateModel>();
+            CreateMap<PhotoUpdatedModel, Photo>();
+            CreateMap<PhotoCreateModel, Photo>();
+        }
+    }
+}
